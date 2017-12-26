ba3e97976878ff61cad07d41ed7ff2951cd94db0
Davut Jepbarov <davut.jepbarov@loodos.com>
2017-12-26 13:08:21 +0300

    Initial Commit

---
+++ KipIt/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  KipIt
+//
+//  Created by djepbarov on 26.12.2017.
+//  Copyright © 2017 djepbarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
